a9bef4abae538851c7502617783bbe57d1a6a353
ohsung0722 <99doldol@gmail.com>
2024-12-23 21:38:13 +0900

    [ADD] zustand 설치 및 userInfo 전역 상태 코드 생성

--- src/components/auth/AuthForms.tsx
+++ src/components/auth/AuthForms.tsx
@@ -4,7 +4,7 @@ type FormData = {
   id: string;
   password: string;
   username?: string;
-  club?: string;
+  studentNumber?: string;
 };
 
 export default function AuthForms() {
@@ -13,7 +13,7 @@ export default function AuthForms() {
     id: "",
     password: "",
     username: "",
-    club: "",
+    studentNumber: "",
   });
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -54,18 +54,18 @@ export default function AuthForms() {
                   ></input>
                 </div>
                 <div>
-                  <label htmlFor="club" className="sr-only">
-                    동아리
+                  <label htmlFor="studentNumber" className="sr-only">
+                    학번
                   </label>
                   <input
                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                    id="club"
-                    name="club"
+                    id="studentNumber"
+                    name="studentNumber"
                     type="text"
                     required={!isLogin}
-                    value={formData.club}
+                    value={formData.studentNumber}
                     onChange={onChange}
-                    placeholder="동아리"
+                    placeholder="학번"
                   ></input>
                 </div>
               </div>
